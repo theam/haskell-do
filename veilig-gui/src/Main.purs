0857036cb07def98623e7cef3abed01f4a369059
Nikita Tchayka <nikitatchayka@gmail.com>
2016-11-03 14:01:29 +0000

    Add basic Pux app

--- veilig-gui/src/Main.purs
+++ veilig-gui/src/Main.purs
@@ -1,9 +1,37 @@
 module Main where
 
-import Prelude
+import Prelude hiding (div)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Signal.Channel (CHANNEL)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Pux (renderToDOM, fromSimple, start)
+import Pux.Html (Html, text, button, span, div)
+import Pux.Html.Events (onClick)
+
+data Action = Increment | Decrement
+
+type State = Int
+
+update :: Action -> State -> State
+update Increment count = count + 1
+update Decrement count = count - 1
+
+view :: State -> Html Action
+view count =
+  div
+    []
+    [ button [ onClick (const Increment) ] [ text "Increment" ]
+    , span [] [ text (show count) ]
+    , button [ onClick (const Decrement) ] [ text "Decrement" ]
+    ]
+
+main :: forall e. Eff (err :: EXCEPTION, channel :: CHANNEL | e) Unit
 main = do
-  log "Hello sailor!"
+  app <- start
+    { initialState: 0
+    , update: fromSimple update
+    , view: view
+    , inputs: []
+    }
+  renderToDOM "#app" app.html
