fca500039b862f7bf0e0ef75ec6a68cf81102c16
Samuel Schlesinger <sgschlesinger@gmail.com>
2016-11-30 14:25:46 -0500

    tested

--- veilig-core/app/Interpreter.hs
+++ veilig-core/app/Interpreter.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Interpreter where
+module Interpreter 
+  ( runInterpreter
+  , notebookInterpreter
+  ) where
 
 import Types
 import Language.Haskell.Interpreter
@@ -7,14 +10,16 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import Data.String.Conversions
 
-intNotebook :: Notebook -> Text
-intNotebook= T.intercalate "\n"
-              . map cellContent
-              . filter (\c -> cellType c == CodeCell )
-              . cells
-
-notebookInterpreter :: Text -> Interpreter Text
+notebookInterpreter :: Notebook -> Interpreter Notebook
 notebookInterpreter code = do
     setImportsQ[("Prelude",Nothing)]
-    a <- eval $ cs code
-    return $ cs a
+    let newIds = [maximum (cellId <$> cells code) + 1 ..]
+    newCells <- sequence $ do
+      (c, newId) <- cells code `zip` newIds
+      case cellType c of
+        DisplayCell -> []
+        TextCell -> pure $ pure c
+        CodeCell -> do
+          let str = cs <$> eval (cs $ cellContent c)
+          [pure c, Cell DisplayCell newId <$> str]
+    return $ code { cells = newCells}
