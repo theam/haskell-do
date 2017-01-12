f8a8c6a4fa3a7668d0d93cbf6910aabc4587200d
Nikita Tchayka <Nikita Tchayka>
2017-01-12 19:05:30 +0000

    Add console to column

--- veilig-gui/src/View.purs
+++ veilig-gui/src/View.purs
@@ -4,7 +4,7 @@ import Prelude (($))
 
 import Pux.Html.Attributes (className)
 import Types
-import Pux.Html (Html, div)
+import Pux.Html (Html, div, nav)
 import Navbar.View as Navbar
 import Cells.View as Cells
 import Console.View as Console
@@ -21,11 +21,30 @@ boilerplate content =
             ]
         ]
 
+columns :: Html Action -> Html Action -> Html Action
+columns leftSide rightSide =
+    div
+        [ className "container-fluid" ]
+        [ div
+            [ className "row" ]
+            [ div
+                [ className "col-lg-4 col-lg-push-8"]
+                [ nav
+                    [ className "navbar navbar-default navbar-fixed-side" ]
+                    [ rightSide ]
+                ]
+            , div
+                [ className "col-lg-8 col-lg-pull-4" ]
+                [ leftSide ]
+            ]
+        ]
+
 view :: AppState -> Html Action
 view appState =
     div
         []
         [ Navbar.view appState
-        , boilerplate $ Cells.view appState
-        , boilerplate $ Console.view appState
+        , columns 
+            (Cells.view appState)
+            (Console.view appState)
         ]
