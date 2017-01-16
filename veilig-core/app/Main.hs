3199d3f19f596c696a3fcaf4554b4c1bb29b00c0
Kit Freddura <kit@theagilemonkeys.com>
2017-01-16 15:53:14 -0800

    working on cradles

--- veilig-core/app/Main.hs
+++ veilig-core/app/Main.hs
@@ -2,9 +2,11 @@ module Main where
 
 import Types
 import WebSocketServer
+import Language.Haskell.GhcMod
 import qualified Network.WebSockets as WS
 import Control.Concurrent
 import System.Environment
+import System.Command (runInteractiveCommand)
 
 address :: String
 address = "0.0.0.0"
@@ -15,5 +17,6 @@ port = 3000
 main :: IO ()
 main = do
   filepath : _ <- getArgs
-  state <- initializeState filepath
+  cradle <- findCradle filepath
+  state <- initializeState filepath cradle
   WS.runServer address port (application state)
