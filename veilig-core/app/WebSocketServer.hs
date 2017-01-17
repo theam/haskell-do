235fb41835e912e9633d40b62b77f8048c492fe5
Kit Freddura <kit@theagilemonkeys.com>
2017-01-17 11:08:07 -0800

    refactored notebook filepate in initializeState

--- veilig-core/app/WebSocketServer.hs
+++ veilig-core/app/WebSocketServer.hs
@@ -6,6 +6,7 @@ import Types
 import Language.Haskell.Interpreter
 import Data.Monoid (mappend, (<>))
 import Data.Text (Text)
+import Data.List (intercalate)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Control.Concurrent
@@ -27,14 +28,13 @@ broadcast conn msg = do
   T.putStrLn ("Log:" <> cs msg)
   WS.sendTextData conn msg
 
--- cradleProject, cradleCurrentDir, cradleRootDir, cradleTempDir, cradleCabalFile, cradleDistDir
 initializeState :: String -> IO State
 initializeState st = do
   (inp, out, err, pid) <- runInteractiveCommand "stack repl"
   hSetBinaryMode inp False
   hSetBinaryMode out False
   hSetBinaryMode err False
-  hPutStrLn inp (":l " ++ st ++ [pathSeparator] ++ "app" ++ [pathSeparator] ++ "Main.hs")
+  hPutStrLn inp (":l " ++ (intercalate [pathSeparator] [st, "app", "Main.hs"]))
   hPutStrLn inp "set prompt \">\""
   hFlush inp
   clearHandle out
@@ -43,7 +43,7 @@ initializeState st = do
   , ghciOutput = out
   , ghciError = err
   , ghciProcessHandle = pid
-  , notebookFilePath = (st ++ [pathSeparator] ++ "app" ++ [pathSeparator] ++ "Main.hs")
+  , notebookFilePath = (intercalate [pathSeparator] [st, "app", "Main.hs"])
   , notebookAuthor = Nothing })
 
 application :: State -> WS.ServerApp
