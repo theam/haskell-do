bd394e4370dcaa42276cbb577d9925bb157acffd
Samuel Schlesinger <sgschlesinger@gmail.com>
2017-01-27 18:51:47 -0500

    main-------

--- veilig-core/app/Main.hs
+++ veilig-core/app/Main.hs
@@ -1,33 +1,12 @@
 module Main where
 
-import Types
 import WebSocketServer
-import qualified Network.WebSockets as WS
-import Control.Concurrent
-import System.Environment
-import System.Process
+import Types
 import Utils
-import Data.IORef
+import qualified Network.WebSockets as WS
+import System.Environment
 
-address :: String
-address = "0.0.0.0"
-
-port :: Int
-port = 3000
-
-main :: IO ()
 main = do
-  projectname : _ <- getArgs
-  -- ^ The user passes their project's name to the command line
-  -- when starting the Veilig server
-  -- - Sam
-  pn <- return $ ProjectName { getProjName = projectname }
-  state <- newIORef State {}
-  createNewProject state pn
-  -- above creates a new project on running the app
-  -- this is placeholder functionality
-  -- - Kit
-  state' <- readIORef state
-  WS.runServer address port (application state')
-  -- ^ Then we run the application based on our state
-  -- - Sam
+  filepath : _ <- getArgs
+  state <- initializeState filepath
+  WS.runServer "0.0.0.0" 3000 (application state)
