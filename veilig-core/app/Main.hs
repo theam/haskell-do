bf3f1346431a7ca8c450d12878541d9a93c5d671
Kit Freddura <kit@theagilemonkeys.com>
2017-01-27 15:56:12 -0800

    made main like old main

--- veilig-core/app/Main.hs
+++ veilig-core/app/Main.hs
@@ -6,7 +6,13 @@ import Utils
 import qualified Network.WebSockets as WS
 import System.Environment
 
+address :: String
+address = "0.0.0.0"
+
+port :: Int
+port = 3000
+
 main = do
   filepath : _ <- getArgs
   state <- initializeState filepath
-  WS.runServer "0.0.0.0" 3000 (application state)
+  WS.runServer address port (application state)
