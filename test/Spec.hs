b814db4660eff0d24769f38ec7baf9cc56df3586
Nikita Tchayka <nikitatchayka@gmail.com>
2017-05-08 11:58:50 +0100

    Fix spec

--- test/Spec.hs
+++ test/Spec.hs
@@ -14,7 +14,6 @@
  - See the License for the specific language governing permissions and
  - limitations under the License.
  -}
-{-# Language NoImplicitPrelude #-}
 module Main where
 
 import Flow
